77a256f55725de71695447857eaddd9a1df7b9cb
tic-top <kirp@umich.edu>
2024-10-22 11:30:14 -0400

    [kirp] account

--- submit9.sh
+++ submit9.sh
@@ -9,7 +9,7 @@
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=1g
 #SBATCH --time=00:05:00
-#SBATCH --account=eecs587f24_class
+#SBATCH --account=cse587f24_class
 #SBATCH --partition=standard
 
 # Run your program
